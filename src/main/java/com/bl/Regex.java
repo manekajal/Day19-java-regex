fc375748c2676d4ea11930c95ab04bc58e5ecfb8
kajal <manekajal2014@gmail.com>
2021-10-06 14:29:53 +0530

    [Kajal]: Refactored Java Regex:

--- src/main/java/com/bl/Regex.java
+++ src/main/java/com/bl/Regex.java
@@ -1,8 +1,30 @@
 package com.bl;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
+
+    public static void uc1() {
+        String firstName = "Kaj";
+
+        String pattern = "^[A-z]{3}";
+        Pattern r = Pattern.compile(pattern);
+
+        Matcher m = r.matcher(firstName);
+
+
+        if (m.matches()) {
+            System.out.println("Matched Pattern:");
+
+        } else {
+            System.out.println(" No Matched :");
+        }
+    }
 
     public static void main(String[] args){
         System.out.println(" java Regex :");
+        System.out.println();
+        uc1();
     }
 }
